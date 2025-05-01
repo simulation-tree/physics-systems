6f3303a38e81c27b8239d9e7079a4f40e2b3685d
Phill <philldamaskin@gmail.com>
2025-05-01 12:33:42 -0400

    add test checking raycast against ground

--- tests/RaycastTests.cs
+++ tests/RaycastTests.cs
@@ -5,12 +5,48 @@ using System;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using Transforms;
+using Unmanaged;
 using Worlds;
 
 namespace Physics.Tests
 {
     public class RaycastTests : PhysicsSystemsTests
     {
+        [Test]
+        public unsafe void RaycastAgainstStaticFloor()
+        {
+            Body floorBody = new(world, new CubeShape(0.5f), BodyType.Static);
+            Transform floorTransform = floorBody;
+            floorTransform.LocalScale = new(100, 1, 1);
+            floorTransform.LocalPosition = new(0, -2, 0);
+
+            simulator.Update(TimeSpan.FromSeconds(0.01f));
+
+            using MemoryAddress temp = MemoryAddress.AllocateValue<(uint, bool)>(default);
+            ref (uint entity, bool hit) result = ref temp.Read<(uint, bool)>();
+            simulator.TryHandleMessage(new RaycastRequest(world, Vector3.Zero, -Vector3.UnitY, new(&HitCallback), 1.501f, (ulong)temp.Address));
+            Assert.That(result.hit, Is.True);
+
+            [UnmanagedCallersOnly]
+            static void HitCallback(RaycastHitCallback.Input input)
+            {
+                World world = input.world;
+                RaycastRequest raycast = input.request;
+                MemoryAddress temp = new((void*)raycast.userData);
+                ref (uint entity, bool hit) result = ref temp.Read<(uint, bool)>();
+                ReadOnlySpan<RaycastHit> hits = input.Hits;
+                if (hits.Length == 0)
+                {
+                    result.hit = false;
+                    return;
+                }
+
+                RaycastHit hit = hits[0];
+                result.hit = true;
+                result.entity = hit.entity;
+            }
+        }
+
         [Test]
         public unsafe void RaycastWithResults()
         {
